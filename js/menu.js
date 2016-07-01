59d847e6814792bb325124518c42d996229779f2
Anna <anna@safronovs.com>
2016-07-02 00:45:51 +0500

    added new class

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,28 @@
 "use strict";
 
-// class Menu {
-// 	constructor(options) {
-// 		this.el = options.el;
+class NewBook {
+	constructor(options) {
+		this.el = options.el;
 
-// 		this._initEvents();
-// 	}
+		this._initEvents();
+	}
 
-// 	_initEvents () {
-// 		this.el.addEventListener('click', this._onMenuClick.bind(this));
-// 	}
+	_initEvents () {
+		this.el.addEventListener('click', this._onMenuClick.bind(this));
+	}
 
 
-// 	_onMenuClick (event) {
-// 		let isItemClick = false;
-// 		if (event.target.classList.contains('menu__item')) {
-// 			isItemClick = true;
-// 			this._onMenuItemClick(event);
-// 		}
+	_onMenuClick (event) {
+		let isItemClick = false;
+		if (event.target.classList.contains('menu__item')) {
+			isItemClick = true;
+			this._onMenuItemClick(event);
+		}
 
-// 		if (!isItemClick) {
-// 			this.el.classList.toggle('menu_open');
-// 		} 
+		if (!isItemClick) {
+			this.el.classList.toggle('menu_open');
+		} 
 		
-// 	}
+	}
 
-
-// 	_onMenuItemClick (event) {
-// 		console.log(event.target);
-// 	}
-
-// }
+}
